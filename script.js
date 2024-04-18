2526a306670ab6be51e5a2f12f56798ebd56c5c5
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-04-17 18:28:38 -0700

    Added two main power ups, boost value and super click. Also created a timing class to keep track of the time elapsed during the game.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+class Time {
+    constructor() {
+        this._gameStartTime = Date.now();
+        this._powerUpStartTime;
+    }
+
+    elapsedTime() {
+        return Math.floor((Date.now() - this._gameStartTime) / 1000);
+    }
+
+    powerUpStartTime() {
+        this._powerUpStartTime = Date.now();
+    }
+    
+    powerUpElapsedTime() {
+        return Math.floor((Date.now() - this._powerUpStartTime) / 1000);
+    }
+}
+
 class CookieCrumb {
     constructor() {
         this._currentCookies = 0;
@@ -26,19 +45,69 @@ class CookieCrumb {
 class PlayThrough {
     constructor() {
       this._cookie = new CookieCrumb();
+      this._timer = new Time();
+      this._cookieCrumbValue = 1;
+      this._holdEnabled = false;
+      this._powerUpSuperClickActive = false;
+      this._cookieCrumb = document.getElementById('cookie-crumb');
+      this._timeElapsed = document.getElementById('time-elapsed');
       
-      this._loadEventListeners();
+      this._loadDefaultEventListeners();
+      this._updateTime();
     }
 
-    _loadEventListeners() {
-        const cookieCrumb = document.getElementById('cookie-crumb').addEventListener('click', this._userClick.bind(this));
+    _loadDefaultEventListeners() {
+        this._cookieCrumb.addEventListener('click', this._userClick.bind(this));
+        const boostValueBtn = document.getElementById('boost-value').addEventListener('click', this._powerUpBoostValue.bind(this));
+        const superClickBtn = document.getElementById('super-click').addEventListener('click', this._powerUpSuperClickToggle.bind(this, true));
+        const freezeTimeBtn = document.getElementById('freeze-time').addEventListener('click', this._powerUpFreezeTime.bind(this))
     }
 
     _userClick() {
         this._cookie.increaseClicks();
-        this._cookie.increaseCookies(2);
+        this._cookie.increaseCookies(this._cookieCrumbValue);
 
         this._updateScoreBoard();
+    }
+
+    _powerUpFreezeTime() {
+        console.log(this._timer.elapsedTime());
+    }
+
+    _powerUpSuperClickToggle(value) {
+        this._powerUpSuperClickActive = value;
+        console.log(this._powerUpSuperClickActive);
+
+        if (value) {
+            this._cookieCrumb.addEventListener('mousedown', this._powerUpSuperClickHold.bind(this, true), {once: true});
+            this._cookieCrumb.addEventListener('mouseup', this._powerUpSuperClickHold.bind(this, false), {once: true});
+        }
+
+        this._timer.powerUpStartTime();
+    }
+
+    _powerUpSuperClickHold(value) {
+        this._holdEnabled = value;
+        this._powerUpSuperClick();
+
+        if (this._timer.powerUpElapsedTime() >= 10) {
+            this._powerUpSuperClickToggle(false);
+        }
+    }
+
+    _powerUpSuperClick() {
+        if (this._holdEnabled) {
+            this._userClick();
+            if (this._timer.powerUpElapsedTime() <= 10) {
+                console.log(this._timer.powerUpElapsedTime())
+                setTimeout(() => {this._powerUpSuperClick()}, 250)
+            };
+        };
+    };
+
+    _powerUpBoostValue() {
+        let boostValue = Math.ceil(Math.random() * 2) + 1;
+        this._cookieCrumbValue = boostValue;
     }
 
     _updateScoreBoard() {
@@ -47,8 +116,13 @@ class PlayThrough {
 
         const totalClicksInfo = document.getElementById('total-clicks');
         totalClicksInfo.textContent = this._cookie.totalClicks;
+    }
 
-
+    _updateTime() {
+        setTimeout(() => {
+            this._timeElapsed.innerText = `00:${this._timer.elapsedTime()}`
+            this._updateTime();
+        }, 1000)
     }
 };
 
